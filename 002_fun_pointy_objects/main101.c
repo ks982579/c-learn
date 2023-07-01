97090b602237708083cf209c26b8dedad40e7f55
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-07-01 21:11:40 +0100

    Adding more from reading. Starting w/pointers

---
+++ 002_fun_pointy_objects/main101.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Every type is either _object_ or _function_
+// Also, pointers = address; derived from reference type
+// Object and function pointers are different
+
+void swap(int, int); // defined in a minute
+
+int main(void) {
+  int a = 21;
+  int b = 17;
+
+  swap(a, b);
+  printf("main: a = %d, b = %d\n", a, b);
+  return 0;
+}
+
---
+++ README.md
@@ -0,0 +1,13 @@
+```yaml
+title: Effective C
+subtitle: An Introduction to Professional C Programming
+author: Robert C. Seacord
+publisher: No Starch Press
+year: 2020
+```
+
+## Ch. 1 Getting Started With C
+
+## Ch.2 Objects, Functions, And Types
+p. 13
+
