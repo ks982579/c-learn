b0cbe17dcf86fbc74ab8bda7ebd3a32d74ce27ee
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-07-05 22:13:45 +0100

    Quick fix and left todo

--- 002_fun_pointy_objects/main102.c
+++ 002_fun_pointy_objects/main102.c
@@ -14,7 +14,7 @@ void update_struct(Human person, char fname[20], char lname[20])
 {
     strcpy(person.first_name, fname);
     strcpy(person.last_name, lname);
-    printf("Hello %s %s", person.first_name, person.last_name);
+    printf("Hello %s %s\n", person.first_name, person.last_name);
 }
 
 int main(void)
@@ -25,6 +25,8 @@ int main(void)
     strcpy(person.last_name, "Doe");
 
     update_struct(person, "Timmy", "Blunt");
-    printf("Hello %s %s", person.first_name, person.last_name);
+    printf("Hello %s %s\n", person.first_name, person.last_name);
     return 0;
 }
+
+// Must fix terrible code
